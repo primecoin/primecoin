382150ad79c80e15fe6d21d030c1e29973b9d569
ankang1 <ankang@v.systems>
2019-10-30 16:45:44 +0800

    delete the printf line

--- src/main.cpp
+++ src/main.cpp
@@ -1662,7 +1662,6 @@ bool CBlock::ConnectBlock(CValidationState &state, CBlockIndex* pindex, CCoinsVi
     //transaction fee will be destroyed after 1st January 2020 in the mainnet, and 1st December 2019 in the testnet
     int64 nDestroyFeeSwitchTime = 1577808000;
     if(fTestNet) nDestroyFeeSwitchTime = 1575129600;
-    printf("block time is %ld, net %d\n", pindex->nTime, fTestNet);
     if(pindex->nTime >= nDestroyFeeSwitchTime)
         nFees = 0;
 
